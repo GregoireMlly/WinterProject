a653030a93028c9ad9dec049d0a59b20408d5157
Greygoooz <76434104+Greygoooz@users.noreply.github.com>
2021-04-08 15:54:56 +0200

    first push

---
+++ Assets/Dé/button_script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class button_script : MonoBehaviour
+{
+    public UnityEvent ButtonClick;
+    void Awake()
+    {
+        if (ButtonClick==null)
+        {
+            ButtonClick = new UnityEvent();
+        }
+    }
+   void OnMouseUp()
+    {
+        int dé_roll = Random.Range(1, 7);
+    }
+}
