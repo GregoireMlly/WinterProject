1c733528563118288d76621052be342192136432
Greygoooz <76434104+Greygoooz@users.noreply.github.com>
2021-04-21 14:39:10 +0200

    common push

---
+++ [Projet S2] Map/Assets/Dé/animate_de.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animate_de : MonoBehaviour
+{
+    public Material nMaterial;
+    public void setImageToNext()   
+    {
+        nMaterial.SetColor("_Color", Color.black);
+    }
+}
--- [Projet S2] Map/Assets/Dé/button_script.cs
+++ [Projet S2] Map/Assets/Dé/button_script.cs
@@ -5,7 +5,12 @@ using UnityEngine.Events;
 
 public class button_script : MonoBehaviour
 {
+    //Material m_Material;
     public UnityEvent ButtonClick;
+    void Start()
+    {
+       // m_Material = GetComponent<Renderer>().material;
+    }
     void Awake()
     {
         if (ButtonClick==null)
@@ -15,6 +20,8 @@ public class button_script : MonoBehaviour
     }
    void OnMouseUp()
     {
+        //m_Material = SetTexture;
         int dé_roll = Random.Range(1, 7);
+        
     }
 }
